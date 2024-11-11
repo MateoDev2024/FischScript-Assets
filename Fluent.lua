289c717df9568c4c588e0b6fec4cfbc3ee839e38
Mateo <159031831+MateoDev2024@users.noreply.github.com>
2024-11-11 21:34:02 +0000

    Update Fluent.lua

--- Fluent.lua
+++ Fluent.lua
@@ -4373,7 +4373,7 @@ local aa = {
                 ["lucide-x-square"] = "rbxassetid://10747384217",
                 ["lucide-zoom-in"] = "rbxassetid://10747384552",
                 ["lucide-zoom-out"] = "rbxassetid://10747384679",
-                ["lucide-fish"] = "rbxassetid://114632400150304"
+                ["lucide-fish"] = "rbxassetid://111705958484352"
             }
         }
     end,

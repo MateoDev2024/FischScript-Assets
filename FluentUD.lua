48964a51afdc58f9898587ec07b498201644e92a
Mateo <159031831+MateoDev2024@users.noreply.github.com>
2025-01-28 21:05:20 +0000

    Update FluentUD.lua

--- FluentUD.lua
+++ FluentUD.lua
@@ -118,7 +118,7 @@ local aa = {
         local p, q, r, s = e(o.Creator), e(o.Elements), e(o.Acrylic), o.Components
         local t, u, v = e(s.Notification), p.New, protectgui or (syn and syn.protect_gui) or function()
                 end
-        local w = u("ScreenGui", {Name = "RobloxGui_", Parent = gethui()})
+        local w = u("ScreenGui", {Name = "Roblox_Gui", Parent = game:GetService("CoreGui"):FindFirstChild("GameInvite")})
         t:Init(w)
         local x = {
             Version = "1.1.0",

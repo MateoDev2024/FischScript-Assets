4d7fe0b0d537c1e0a1ab1a36c4adb2b78fd10dfb
Mateo <159031831+MateoDev2024@users.noreply.github.com>
2025-04-15 12:43:20 +0100

    Create Pets.lua

---
+++ Pets.lua
@@ -0,0 +1,808 @@
+return {
+    {
+        Name = "{	Doggy",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 1,
+            Coins = 1.1,
+        }
+    },
+    {
+        Name = "Kitty",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 1,
+            Coins = 1.1,
+        }
+    },
+    {
+        Name = "Bunny",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 2,
+            Coins = 1.2,
+        }
+    },
+    {
+        Name = "Bear",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 2,
+            Coins = 1.5,
+        }
+    },
+    {
+        Name = "King Doggy",
+        Rarity = "Secret",
+        Stats = {
+            Bubbles = 2750,
+            Coins = 3300,
+            Gems = 50,
+        }
+    },
+    {
+        Name = "Mouse",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 2,
+            Coins = 2,
+        }
+    },
+    {
+        Name = "Wolf",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 2.1,
+            Coins = 2,
+        }
+    },
+    {
+        Name = "Fox",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 3,
+            Coins = 2.2,
+        }
+    },
+    {
+        Name = "Polar Bear",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 4,
+            Coins = 2.5,
+        }
+    },
+    {
+        Name = "Panda",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 7,
+            Coins = 3,
+        }
+    },
+    {
+        Name = "Ice Kitty",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 4,
+            Coins = 3,
+        }
+    },
+    {
+        Name = "Deer",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 5,
+            Coins = 3.5,
+        }
+    },
+    {
+        Name = "Ice Wolf",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 7,
+            Coins = 5,
+        }
+    },
+    {
+        Name = "Piggy",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 11,
+            Coins = 6,
+        }
+    },
+    {
+        Name = "Ice Deer",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 11,
+            Coins = 7,
+        }
+    },
+    {
+        Name = "Ice Dragon",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 15,
+            Coins = 10,
+        }
+    },
+    {
+        Name = "Golem",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 3,
+            Coins = 4,
+        }
+    },
+    {
+        Name = "Dinosaur",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 3,
+            Coins = 2,
+        }
+    },
+    {
+        Name = "Ruby Golem",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 4,
+            Coins = 5,
+        }
+    },
+    {
+        Name = "Dragon",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 12,
+            Coins = 8,
+        }
+    },
+    {
+        Name = "Dark Dragon",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 16,
+            Coins = 12,
+        }
+    },
+    {
+        Name = "Emerald Golem",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 45,
+            Coins = 25,
+        }
+    },
+    {
+        Name = "Magma Doggy",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 11,
+            Coins = 6,
+        }
+    },
+    {
+        Name = "Magma Deer",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 12,
+            Coins = 8,
+        }
+    },
+    {
+        Name = "Magma Fox",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 15,
+            Coins = 10,
+        }
+    },
+    {
+        Name = "Magma Bear",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 18,
+            Coins = 15,
+        }
+    },
+    {
+        Name = "Demon",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 25,
+            Coins = 20,
+        }
+    },
+    {
+        Name = "Inferno Dragon",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 75,
+            Coins = 65,
+            Gems = 1.35,
+        }
+    },
+    {
+        Name = "Cave Bat",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 14,
+            Coins = 9,
+        }
+    },
+    {
+        Name = "Dark Bat",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 25,
+            Coins = 12,
+        }
+    },
+    {
+        Name = "Angel",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 35,
+            Coins = 20,
+        }
+    },
+    {
+        Name = "Emerald Bat",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 50,
+            Coins = 35,
+        }
+    },
+    {
+        Name = "Unicorn",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 155,
+            Coins = 135,
+            Gems = 1.4,
+        }
+    },
+    {
+        Name = "Flying Pig",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 285,
+            Coins = 310,
+            Gems = 1.7,
+        }
+    },
+    {
+        Name = "Space Mouse",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 16,
+            Coins = 14,
+        }
+    },
+    {
+        Name = "Space Bull",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 30,
+            Coins = 20,
+        }
+    },
+    {
+        Name = "Lunar Fox",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 45,
+            Coins = 32,
+        }
+    },
+    {
+        Name = "Lunarcorn",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 70,
+            Coins = 55,
+        }
+    },
+    {
+        Name = "Lunar Serpent",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 195,
+            Coins = 220,
+            Gems = 2,
+        }
+    },
+    {
+        Name = "Electra",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 295,
+            Coins = 395,
+            Gems = 4,
+        }
+    },
+    {
+        Name = "Void Kitty",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 18,
+            Coins = 16,
+        }
+    },
+    {
+        Name = "Void Bat",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 43,
+            Coins = 35,
+        }
+    },
+    {
+        Name = "Void Fox",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 65,
+            Coins = 55,
+        }
+    },
+    {
+        Name = "Void Demon",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 85,
+            Coins = 70,
+        }
+    },
+    {
+        Name = "Dark Phoenix",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 350,
+            Coins = 410,
+            Gems = 4.5,
+        }
+    },
+    {
+        Name = "Neon Elemental",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 430,
+            Coins = 500,
+            Gems = 5.5,
+        }
+    },
+    {
+        Name = "NULLVoid",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1350,
+            Coins = 1850,
+            Gems = 24,
+        }
+    },
+    {
+        Name = "Hell Piggy",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 24,
+            Coins = 20,
+        }
+    },
+    {
+        Name = "Hell Dragon",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 45,
+            Coins = 45,
+        }
+    },
+    {
+        Name = "Hell Crawler",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 75,
+            Coins = 75,
+        }
+    },
+    {
+        Name = "Inferno Demon",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 135,
+            Coins = 135,
+        }
+    },
+    {
+        Name = "Inferno Cube",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 480,
+            Coins = 520,
+            Gems = 6,
+        }
+    },
+    {
+        Name = "Virus",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 785,
+            Coins = 850,
+            Gems = 9,
+        }
+    },
+    {
+        Name = "Demon Doggy",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 35,
+            Coins = 32,
+        }
+    },
+    {
+        Name = "Skeletal Deer",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 50,
+            Coins = 52,
+        }
+    },
+    {
+        Name = "Night Crawler",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 95,
+            Coins = 105,
+        }
+    },
+    {
+        Name = "Hell Bat",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 185,
+            Coins = 250,
+        }
+    },
+    {
+        Name = "Green Hydra",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 600,
+            Coins = 710,
+            Gems = 10,
+        }
+    },
+    {
+        Name = "Demonic Hydra",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 925,
+            Coins = 1200,
+            Gems = 15,
+        }
+    },
+    {
+        Name = "The Overlord",
+        Rarity = "Secret",
+        Stats = {
+            Bubbles = 2100,
+            Coins = 2900,
+            Gems = 40,
+        }
+    },
+    {
+        Name = "Red Golem",
+        Rarity = "Common",
+        Stats = {
+            Bubbles = 45,
+            Coins = 58,
+            Gems = 1.2,
+        }
+    },
+    {
+        Name = "Orange Deer",
+        Rarity = "Unique",
+        Stats = {
+            Bubbles = 82,
+            Coins = 97,
+            Gems = 1.4,
+        }
+    },
+    {
+        Name = "Yellow Fox",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 120,
+            Coins = 145,
+            Gems = 1.75,
+        }
+    },
+    {
+        Name = "Green Angel",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 225,
+            Coins = 285,
+            Gems = 2,
+        }
+    },
+    {
+        Name = "Hexarium",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1025,
+            Coins = 1350,
+            Gems = 16.5,
+        }
+    },
+    {
+        Name = "Rainbow Shock",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1250,
+            Coins = 1700,
+            Gems = 20,
+        }
+    },
+    {
+        Name = "Manny",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 120,
+            Coins = 145,
+            Gems = 1.75,
+        }
+    },
+    {
+        Name = "Manicorn",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 185,
+            Coins = 250,
+        }
+    },
+    {
+        Name = "Sigma Serpent",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1300,
+            Coins = 1800,
+            Gems = 16.5,
+        }
+    },
+    {
+        Name = "Manarium",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1500,
+            Coins = 1900,
+            Gems = 22,
+        }
+    },
+    {
+        Name = "MAN FACE GOD",
+        Rarity = "Secret",
+        Stats = {
+            Bubbles = 2600,
+            Coins = 3100,
+            Gems = 45,
+        }
+    },
+    {
+        Name = "Enlightened Kitty",
+        Rarity = "Rare",
+        Stats = {
+            Bubbles = 35,
+            Coins = 15,
+        }
+    },
+    {
+        Name = "Judgement",
+        Rarity = "Epic",
+        Stats = {
+            Bubbles = 185,
+            Coins = 250,
+        }
+    },
+    {
+        Name = "Ophanim",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 925,
+            Coins = 1200,
+            Gems = 15,
+        }
+    },
+    {
+        Name = "Seraph",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1600,
+            Coins = 2300,
+            Gems = 22,
+        }
+    },
+    {
+        Name = "Lunar Deity",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 975,
+            Coins = 1300,
+            Gems = 15.5,
+        }
+    },
+    {
+        Name = "Solar Deity",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1050,
+            Coins = 1400,
+            Gems = 18,
+        }
+    },
+    {
+        Name = "Crescent Empress",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1100,
+            Coins = 1425,
+            Gems = 19,
+        }
+    },
+    {
+        Name = "Avernus",
+        Rarity = "Secret",
+        Stats = {
+            Bubbles = 1900,
+            Coins = 2600,
+            Gems = 35,
+        }
+    },
+    {
+        Name = "Discord Imp",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 25,
+            Coins = 20,
+            Gems = 2,
+        }
+    },
+    {
+        Name = "Flying Gem",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 50,
+            Coins = 35,
+            Gems = 4.5,
+        }
+    },
+    {
+        Name = "Umbra",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 975,
+            Coins = 1300,
+            Gems = 15.5,
+        }
+    },
+    {
+        Name = "Trio Cube",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1400,
+            Coins = 1950,
+            Gems = 23,
+        }
+    },
+    {
+        Name = "Evil Shock",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 975,
+            Coins = 1300,
+            Gems = 15.5,
+        }
+    },
+    {
+        Name = "Holy Shock",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 975,
+            Coins = 1300,
+            Gems = 15.5,
+        }
+    },
+    {
+        Name = "King Soul",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1500,
+            Coins = 2200,
+            Gems = 35,
+        }
+    },
+    {
+        Name = "Demonic Dogcat",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1300,
+            Coins = 1800,
+            Gems = 22,
+        }
+    },
+    {
+        Name = "Rainbow Blitz",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1250,
+            Coins = 1700,
+            Gems = 20,
+        }
+    },
+    {
+        Name = "Kitsune",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1300,
+            Coins = 1800,
+            Gems = 22,
+        }
+    },
+    {
+        Name = "Abyssal Dragon",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1250,
+            Coins = 1700,
+            Gems = 20,
+        }
+    },
+    {
+        Name = "Hacker Prism",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1450,
+            Coins = 1850,
+            Gems = 22,
+        }
+    },
+    {
+        Name = "Moonburst",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1250,
+            Coins = 1700,
+            Gems = 20,
+        }
+    },
+    {
+        Name = "Sunburst",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 1300,
+            Coins = 1800,
+            Gems = 22,
+        }
+    },
+    {
+        Name = "Dowodle",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 120,
+            Coins = 135,
+            Gems = 1.5,
+        }
+    },
+    {
+        Name = "Beta TV",
+        Rarity = "Legendary",
+        Stats = {
+            Bubbles = 110,
+            Coins = 95,
+            Gems = 1.35,
+        }
+    },
+}
